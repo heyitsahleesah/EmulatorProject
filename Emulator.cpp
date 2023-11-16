4a73757e076d8830251adba1fdca3795bf47e347
Alissa <alissa.graham88@gmail.com>
2023-11-16 12:03:54 -0800

    began disassembler

---
+++ Emulator.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <vector>
+#include <iomanip>
+
+int Disassemble8080p(unsigned char *codebuffer, int pc)
+{
+    unsigned char *code = &codebuffer[pc];
+    int opbytes = 1;
+    // format an integer as a hexidecimal with the width of 4 characters, padd with leading
+    std::cout << std::hex << std::setw(4) << std::setfill('0') << pc << " ";
+    switch (*code)
+    {
+    case 0x00:
+    case 0x08:
+    case 0x10:
+    case 0x18:
+    case 0x28:
+        std::cout << "NOP";
+        break;
+    // loads a 16-bit value into the BC register pair
+    case 0x01:
+        std::cout << "LXI B,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x02:
+        std::cout << "STAX B";
+        break;
+    case 0x03:
+        std::cout << "INR B";
+        break;
+    case 0x04:
+        std::cout << "INX B";
+        break;
+    case 0x05:
+        std::cout << "DCR B";
+        break;
+    case 0x06:
+        std::cout << "MVI B,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[1];
+        opbytes = 2;
+        break;
+    case 0x07:
+        std::cout << "RLC";
+        break;
+    case 0x09:
+        std::cout << "DAD B";
+        break;
+    case 0x0a:
+        std::cout << "LDAX B";
+        break;
+    case 0x0b:
+        std::cout << "DCX B";
+        break;
+    case 0x0c:
+        std::cout << "INR C";
+        break;
+    case 0x0d:
+        std::cout << "DCR C";
+        break;
+    case 0x0e:
+        std::cout << "MVI C,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[1];
+        opbytes = 2;
+        break;
+    case 0x0f:
+        std::cout << "RRC";
+        break;
+    case 0x11:
+        std::cout << "LXI D,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x12:
+        std::cout << "STAX D";
+        break;
+    case 0x13:
+        std::cout << "INX D";
+        break;
+    case 0x14:
+        std::cout << "INR D";
+        break;
+    case 0x15:
+        std::cout << "DCR D";
+        break;
+    case 0x16:
+        std::cout << "MVI D,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[1];
+        opbytes = 2;
+        break;
+    case 0x17:
+        std::cout << "RAL";
+        break;
+    case 0x19:
+        std::cout << "DAD D";
+        break;
+    case 0x1a:
+        std::cout << "LDAX D";
+        break;
+    case 0x1b:
+        std::cout << "DCX D";
+        break;
+    case 0x1c:
+        std::cout << "INR E";
+        break;
+    case 0x1d:
+        std::cout << "DCR E";
+        break;
+    case 0x1e:
+        std::cout << "MVI E,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[1];
+        opbytes = 2;
+        break;
+    case 0x1f:
+        std::cout << "RAR";
+        break;
+    case 0x20:
+        std::cout << "RIM";
+        break;
+    case 0x21:
+        std::cout << "LXI H,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x22:
+        std::cout << "SHLD $" << std::hex << std::setw(4) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x23:
+        std::cout << "INX H";
+        break;
+    case 0x24:
+        std::cout << "INR H";
+        break;
+    case 0x25:
+        std::cout << "DCR H";
+        break;
+    case 0x26:
+        std::cout << "MVI H,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[1];
+        opbytes = 2;
+        break;
+    case 0x27:
+        std::cout << "DAA";
+        break;
+    case 0x29:
+        std::cout << "DAD H";
+        break;
+    case 0x2a:
+        std::cout << "SHLD $" << std::hex << std::setw(4) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x2b:
+        std::cout << "DCX H";
+        break;
+    case 0x2c:
+        std::cout << "INR L";
+        break;
+    case 0x2d:
+        std::cout << "DCR L";
+        break;
+    case 0x2e:
+        std::cout << "MVI L,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[1];
+        opbytes = 2;
+        break;
+    case 0x2f:
+        std::cout << "CMA";
+        break;
+    case 0x30:
+        std::cout << "SIM";
+        break;
+    case 0x31:
+        std::cout << "LXI SP,#$" << std::hex << std::setw(2) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x32:
+        std::cout << "STA $" << std::hex << std::setw(4) << std::setfill('0') << (int)code[2] << (int)code[1];
+        opbytes = 3;
+        break;
+    case 0x33:
+        std::cout << "INX SP";
+        break;
+    case 0x34:
+        std::cout << "INR M";
+        break;
+    case 0x35:
+        std::cout << "DCR M";
+        break;
+    }
+}
